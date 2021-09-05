88ad1082544c27ef20b24cc9492435c109be8ca0
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-09-05 12:42:36 -0400

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,5 +1,5 @@
 -- Users have a location and it costs energy to move from one location to another.
--- I'm not sure if movement will be immediate it, gradual, or all at once the end of a duration. Probably immediate. 
+-- I'm not sure if movement will be immediate it, gradual, or all at once the end of a duration. Probably immediate.
 -- Energy will cost some currency (maybe I'll use mana)
 -- That energy will also be needed to "launch" your ship (basically register your account as active). How much I'm not sure. Maybe the average of 5 mana pack's worth.
 
@@ -12,4 +12,8 @@ USE OpenComms;
 DELIMITER $$
 
 CREATE TABLE Users(id INT UNSIGNED NOT NULL auto_increment, location POINT NOT NULL SRID 4326, energy INT UNSIGNED NOT NULL DEFAULT 0);
+
+-- ST_Distance_Spheroid is likely going to be very useful for calculating the distance between points, in which case SRID won't matter.
+-- Because this distance is calculated based on the given spheroid, I can just increase the radius of the spheroid as the population grows
+
 DELIMITER ;
